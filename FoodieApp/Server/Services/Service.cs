f95c4cdd9dd309b800d3e0fa84efd75e2a3153df
ajuarez <Alexis.Juarez@Tideworks.com>
2023-02-22 05:44:03 -0600

    review tabs and tests

---
+++ FoodieApp/Server/Services/Service.cs
@@ -0,0 +1,37 @@
+﻿namespace FoodieApp.Server.Services
+{
+    public class Service : IService
+    {
+        private readonly IDependency _dependency;
+
+        public Service(IDependency dependency)
+        {
+            _dependency = dependency;
+        }
+        public string GetNumberOfRetries(bool shouldSuccess)
+        {
+            int retries = _dependency.NumberOfRetries();
+            return shouldSuccess ? $"Hello there are {retries} and counting" : throw new Exception("Something went wrong");
+        }
+    }
+}
+
+    public interface IService
+    {
+        string GetNumberOfRetries(bool shouldSuccess);
+    }
+
+public class Dependency : IDependency
+{
+    public Dependency()
+    {
+            
+    }
+    public int NumberOfRetries() => 3;
+}
+
+public interface IDependency
+{
+    int NumberOfRetries();
+}
+
